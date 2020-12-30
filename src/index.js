1ecc122634e659826a7ca9bfb1b4f2fea86ce929
Justina Kuchta <justyna.kuchta@gmail.com>
2020-12-29 20:41:26 -0500

    working serup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <div>Hello World</div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('app'))
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('app'))
