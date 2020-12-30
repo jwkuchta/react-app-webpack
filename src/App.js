7d4a981a0ef6c911004f1bb7f45ce7a791afae65
Justina Kuchta <justyna.kuchta@gmail.com>
2020-12-30 09:22:45 -0500

    config works on mac but not on windows, see other branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from "react";
+import React, { useRef, useEffect } from "react";
 
-export default class App extends Component {
-  
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+const App = () => {
+
+  // const mapRef = useRef()
+
+  // useEffect(() => {
+    
+  // })
+
+  return (
+    <div className="webmap"> 
+      <h1>Hello World!</h1>
+    </div>
+  );
 }
+
+export default App
